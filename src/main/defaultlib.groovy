fd5cb87f34f99295505aa035cda914f9bb2836d1
GuterGringo <killer_hog@hotmail.de>
2017-08-11 16:00:10 +0200

    Update defaultlib.groovy

--- src/main/defaultlib.groovy
+++ src/main/defaultlib.groovy
@@ -3,3 +3,15 @@ package main;
 def mavenbuild(giturl){
 	echo 'Hello'
 }
+
+def getSource(srcurl, stashname){
+	git url:srcurl
+	stash includes: '*', name: stashname
+}
+
+def getSource(stashname, reponame){
+	sh "'${reponame}/bin/mvn' -B clean install"
+	unstash stashname
+        sh "docker build -t gutergringo/${reponame} ."
+        sh "docker push gutergringo/${reponame}"	
+}
