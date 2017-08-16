4ac20fc2a917e18a2a959dff6f027654a4f0f312
GuterGringo <killer_hog@hotmail.de>
2017-08-16 14:56:34 +0200

    Update defaultlib.groovy

--- src/main/defaultlib.groovy
+++ src/main/defaultlib.groovy
@@ -18,6 +18,15 @@ def deployContainer(dockerhost){
     	sh "docker-compose -H tcp://${dockerhost}:2375 up -d --build"
 }
 
-def buildJar(){
-	
+def defaultJarBuild(srcurl, stashname){
+	def mvnHome = tool 'M3'
+        // Get code from GitHub repository
+        git url:srcurl
+        sh "'${mvnHome}/bin/mvn' -B clean install"
+        
+	// Unit Test
+        sh "${mvnHome}/bin/mvn test"
+        junit 'target/surefire-reports/*.xml'
+        archive 'target/*.jar'
+        stash includes: '**', name: stashname
 }
