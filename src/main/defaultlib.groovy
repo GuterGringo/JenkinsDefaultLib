d9a52ec47170f42fd0ce248e1e255d309f38dfdf
GuterGringo <killer_hog@hotmail.de>
2017-08-16 14:26:27 +0200

    Update defaultlib.groovy

--- src/main/defaultlib.groovy
+++ src/main/defaultlib.groovy
@@ -10,3 +10,10 @@ def publishImage(stashname, dockerrepo, dockerhost){
         sh "docker -H tcp://${dockerhost}:2375 build -t gutergringo/${dockerrepo} ."
         sh "docker -H tcp://${dockerhost}:2375 push gutergringo/${dockerrepo}"	
 }
+
+def deployContainer(dockerhost){
+	def String srcdockercompose = 'https://github.com/GuterGringo/demoapplication.git'
+	git url:srcdockercompose
+    	sh "docker-compose -H tcp://${dockerhost}:2375 pull"
+    	sh "docker-compose -H tcp://${dockerhost}:2375 up -d --build"
+}
