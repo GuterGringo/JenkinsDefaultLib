917f39b2246c52c0427848984c50d767ef577333
GuterGringo <killer_hog@hotmail.de>
2017-11-06 08:20:17 +0100

    Update defaultlib.groovy

--- src/main/defaultlib.groovy
+++ src/main/defaultlib.groovy
@@ -1,16 +1,19 @@
 package main;
 
+// get sourcecode from git-repository
 def getSource(srcurl, stashname){
 	git url:srcurl
 	stash includes: '**', name: stashname
 }
 
+// create and upload images to docker-repository
 def publishImage(stashname, dockerrepo, dockerhost){
 	unstash stashname
         sh "docker -H tcp://${dockerhost}:2375 build -t gutergringo/${dockerrepo} ."
         sh "docker -H tcp://${dockerhost}:2375 push gutergringo/${dockerrepo}"	
 }
 
+// start docker container on target system
 def deployContainer(dockerhost){
 	def String srcdockercompose = 'https://github.com/GuterGringo/demoapplication.git'
 	git url:srcdockercompose
@@ -18,9 +21,9 @@ def deployContainer(dockerhost){
     	sh "docker-compose -H tcp://${dockerhost}:2375 up -d --build"
 }
 
+// build and test Java JAR file from git-repository
 def defaultJarBuild(srcurl, stashname){
 	def mvnHome = tool 'M3'
-        // Get code from GitHub repository
         git url:srcurl
         sh "'${mvnHome}/bin/mvn' -B clean install"
         
